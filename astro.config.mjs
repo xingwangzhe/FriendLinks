a00c555e022fcf0ed62cf4eab2128f458ffb7627
xingwangzhe <xingwangzhe@outlook.com>
2025-12-04 20:05:37 +0800

    Add @edgeone/astro dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+import edgeoneAdapter from '@edgeone/astro';
+
+export default defineConfig({
+	output: 'server',
+	adapter: edgeoneAdapter(),
+});
