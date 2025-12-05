c6cd0c46dbb6e71d55091f3d91b921044a18a4d6
xingwangzhe <xingwangzhe@outlook.com>
2025-12-05 12:01:35 +0800

    feat: 取消注释 edgeone 适配器配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-// import edgeoneAdapter from '@edgeone/astro';
+import edgeoneAdapter from '@edgeone/astro';
 
 export default defineConfig({
 	output: 'static',
-    // adapter: edgeoneAdapter({
-    //     outDir: ".edgeone",
-	// 	includeFiles: ['public/**'],
-	// }),
+    adapter: edgeoneAdapter({
+        outDir: ".edgeone",
+		includeFiles: ['public/**'],
+	}),
 });
