b8e5dbf188825a7dd7dd2e7f72174883f5db6b57
xingwangzhe <xingwangzhe@outlook.com>
2025-12-06 21:27:29 +0800

    fix:

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,8 +19,8 @@ export default defineConfig({
 			},
 		},
 	},
-	adapter: edgeoneAdapter({
-	    outDir: ".edgeone",
-	    includeFiles: ['public/**'],
-	}),
+	// adapter: edgeoneAdapter({
+	//     outDir: ".edgeone",
+	//     includeFiles: ['public/**'],
+	// }),
 });
