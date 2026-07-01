3773ab5eefc6d620a89e4715b39957ef8c9b542a
王兴家 <xingwangzhe@outlook.com>
2026-07-01 16:29:35 +0800

    feat: 服务子域名增加tool/app/dev/stage等前缀
    Signed-off-by: 王兴家 <xingwangzhe@outlook.com>

--- scripts/filter/subdomains.ts
+++ scripts/filter/subdomains.ts
@@ -1,2 +1,2 @@
 /** 子域名服务前缀 */
-export const SERVICE_SUBDOMAINS = /^(cloud|img|cdn|static|assets|media|files?|dl|download|upload|git|status|monitor|nav|wiki|docs?|help|support|m|mobile|news|cmd|mirror|镜像|store|shop|buy|mall|book|print|job|career|draw|paint|design|photo|bbs|forum|社区|club|group|api)[.-]/i;
+export const SERVICE_SUBDOMAINS = /^(cloud|img|cdn|static|assets|media|files?|dl|download|upload|git|status|monitor|nav|wiki|docs?|help|support|m|mobile|news|cmd|mirror|镜像|store|shop|buy|mall|book|print|job|career|draw|paint|design|photo|bbs|forum|社区|club|group|api|tool|tools?|app|www\d?|mail|email|smtp|pop|imap|ftp|dev|stage|staging|test|beta|demo)[.-]/i;
